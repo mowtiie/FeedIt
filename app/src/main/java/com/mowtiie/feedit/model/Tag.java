5c0d1265534ff2c44c5ee88da41e59ec97070e74
mowtiie <mowtiie.dev@gmail.com>
2026-07-10 09:01:52 +0800

    Created tag model class

---
+++ app/src/main/java/com/mowtiie/feedit/model/Tag.java
@@ -0,0 +1,49 @@
+package com.mowtiie.feedit.model;
+
+public class Tag {
+
+    private long id;
+    private String name;
+    private String color;
+    private long createdAt;
+
+    public Tag() {
+    }
+
+    public Tag(String name, String color) {
+        this.name = name;
+        this.color = color;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public long getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(long createdAt) {
+        this.createdAt = createdAt;
+    }
+}
