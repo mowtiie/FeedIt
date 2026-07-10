1f69e8bcadd3fea964fc69b96f79ae1a5364379a
mowtiie <mowtiie.dev@gmail.com>
2026-07-10 09:02:22 +0800

    Created article model class

---
+++ app/src/main/java/com/mowtiie/feedit/model/Article.java
@@ -0,0 +1,125 @@
+package com.mowtiie.feedit.model;
+
+public class Article {
+
+    private long id;
+    private long feedId;
+    private String guid;
+    private String title;
+    private String link;
+    private String author;
+    private String summary;
+    private String content;
+    private String imageUrl;
+    private Long publishedAt;
+    private long fetchedAt;
+    private boolean isRead;
+    private boolean isStarred;
+
+    public Article() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getFeedId() {
+        return feedId;
+    }
+
+    public void setFeedId(long feedId) {
+        this.feedId = feedId;
+    }
+
+    public String getGuid() {
+        return guid;
+    }
+
+    public void setGuid(String guid) {
+        this.guid = guid;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getSummary() {
+        return summary;
+    }
+
+    public void setSummary(String summary) {
+        this.summary = summary;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public Long getPublishedAt() {
+        return publishedAt;
+    }
+
+    public void setPublishedAt(Long publishedAt) {
+        this.publishedAt = publishedAt;
+    }
+
+    public long getFetchedAt() {
+        return fetchedAt;
+    }
+
+    public void setFetchedAt(long fetchedAt) {
+        this.fetchedAt = fetchedAt;
+    }
+
+    public boolean isRead() {
+        return isRead;
+    }
+
+    public void setRead(boolean read) {
+        isRead = read;
+    }
+
+    public boolean isStarred() {
+        return isStarred;
+    }
+
+    public void setStarred(boolean starred) {
+        isStarred = starred;
+    }
+}
