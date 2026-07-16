67f820805dfdb707086a349267869a552edd725f
mowtiie <mowtiie.dev@gmail.com>
2026-07-16 15:20:48 +0800

    Created preferences keys class

---
+++ app/src/main/java/com/mowtiie/feedit/util/PrefsKeys.java
@@ -0,0 +1,14 @@
+package com.mowtiie.feedit.util;
+
+public final class PrefsKeys {
+
+    public static final String PREFS_NAME = "feedit_prefs";
+
+    public static final String SORT_ORDER = "sort_order";
+    public static final String SYNC_WIFI_ONLY = "sync_wifi_only";
+    public static final String NOTIFICATIONS_ENABLED = "notifications_enabled";
+    public static final String DARK_MODE = "dark_mode";
+
+    private PrefsKeys() {
+    }
+}
