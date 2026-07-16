ed979bbbee7892e3bdee2341c81a86d4771ecc40
mowtiie <mowtiie.dev@gmail.com>
2026-07-16 14:25:43 +0800

    Created insets utility class

---
+++ app/src/main/java/com/mowtiie/feedit/ui/util/InsetsUtil.java
@@ -0,0 +1,31 @@
+package com.mowtiie.feedit.ui.util;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public final class InsetsUtil {
+
+    private InsetsUtil() {
+    }
+
+    public static void applyEdgeToEdgeInsets(@NonNull View topBar, @Nullable View bottomContent) {
+        ViewCompat.setOnApplyWindowInsetsListener(topBar, (v, windowInsets) -> {
+            Insets bars = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(bars.left, bars.top, bars.right, v.getPaddingBottom());
+            return windowInsets;
+        });
+
+        if (bottomContent != null) {
+            ViewCompat.setOnApplyWindowInsetsListener(bottomContent, (v, windowInsets) -> {
+                Insets bars = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars());
+                v.setPadding(bars.left, v.getPaddingTop(), bars.right, bars.bottom);
+                return windowInsets;
+            });
+        }
+    }
+}
