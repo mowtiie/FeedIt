06a362a49efdbbd24ca4c532e3f86e3a17aa04f2
mowtiie <mowtiie.dev@gmail.com>
2026-07-18 21:22:24 +0800

    Created additional article layouts, updated strings.xml and PrefKeys class

--- app/src/main/java/com/mowtiie/feedit/util/PrefsKeys.java
+++ app/src/main/java/com/mowtiie/feedit/util/PrefsKeys.java
@@ -4,22 +4,29 @@ public final class PrefsKeys {
 
     public static final String PREFS_NAME = "feedit_prefs";
 
-    public static final String SORT_ORDER = "sort_order"; // used by MainViewModel
-    public static final String SYNC_WIFI_ONLY = "sync_wifi_only"; // used by SyncScheduler
+    public static final String SORT_ORDER = "sort_order";
+    public static final String SYNC_WIFI_ONLY = "sync_wifi_only";
     public static final String NOTIFICATIONS_ENABLED = "notifications_enabled";
-    public static final String DARK_MODE = "dark_mode"; // one of: "system", "light", "dark"
+    public static final String DARK_MODE = "dark_mode";
 
-    public static final String STARTUP_PAGE = "startup_page"; // one of the STARTUP_PAGE_* values below
+    public static final String STARTUP_PAGE = "startup_page";
     public static final String STARTUP_PAGE_ALL = "all";
     public static final String STARTUP_PAGE_UNREAD = "unread";
     public static final String STARTUP_PAGE_STARRED = "starred";
 
-    public static final String CONTRAST_LEVEL = "contrast_level"; // one of the CONTRAST_* values below
+    public static final String CONTRAST_LEVEL = "contrast_level";
     public static final String CONTRAST_STANDARD = "standard";
     public static final String CONTRAST_MEDIUM = "medium";
     public static final String CONTRAST_HIGH = "high";
 
     public static final String DYNAMIC_COLORS_ENABLED = "dynamic_colors_enabled";
+
+    public static final String ARTICLE_LAYOUT_STYLE = "article_layout_style";
+    public static final String LAYOUT_LIST = "list";
+    public static final String LAYOUT_COMPACT = "compact";
+    public static final String LAYOUT_CARD = "card";
+    public static final String LAYOUT_MAGAZINE = "magazine";
+    public static final String LAYOUT_DETAILED = "detailed";
 
     private PrefsKeys() {
     }
